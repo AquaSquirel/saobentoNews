3f0539bac87f4ab3b67c2b8c86b791d2232d0809
AquaSquirel <91857421+AquaSquirel@users.noreply.github.com>
2025-01-03 17:11:45 -0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// Defina o diretório estático para servir arquivos
+app.use(express.static(__dirname));
+
+// Rota para a página inicial
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// Rota para servir qualquer arquivo HTML diretamente
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, req.path));
+});
+
+// Inicie o servidor
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
